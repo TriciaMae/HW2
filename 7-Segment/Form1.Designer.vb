63012798e41eddbe9ca19e7c5bf9197ddb7faf07
TriciaMae <58018345+TriciaMae@users.noreply.github.com>
2020-02-18 11:45:22 +0800

    Vers 01: Design

---
+++ 7-Segment/Form1.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.P1 = New System.Windows.Forms.Panel()
+        Me.P2 = New System.Windows.Forms.Panel()
+        Me.P3 = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.Panel10 = New System.Windows.Forms.Panel()
+        Me.Panel11 = New System.Windows.Forms.Panel()
+        Me.Panel12 = New System.Windows.Forms.Panel()
+        Me.Panel13 = New System.Windows.Forms.Panel()
+        Me.Panel14 = New System.Windows.Forms.Panel()
+        Me.Panel15 = New System.Windows.Forms.Panel()
+        Me.Panel16 = New System.Windows.Forms.Panel()
+        Me.Panel17 = New System.Windows.Forms.Panel()
+        Me.Panel18 = New System.Windows.Forms.Panel()
+        Me.Panel19 = New System.Windows.Forms.Panel()
+        Me.Panel20 = New System.Windows.Forms.Panel()
+        Me.Panel21 = New System.Windows.Forms.Panel()
+        Me.Panel22 = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'P1
+        '
+        Me.P1.Location = New System.Drawing.Point(45, 40)
+        Me.P1.Name = "P1"
+        Me.P1.Size = New System.Drawing.Size(68, 67)
+        Me.P1.TabIndex = 0
+        '
+        'P2
+        '
+        Me.P2.Location = New System.Drawing.Point(119, 40)
+        Me.P2.Name = "P2"
+        Me.P2.Size = New System.Drawing.Size(68, 67)
+        Me.P2.TabIndex = 1
+        '
+        'P3
+        '
+        Me.P3.Location = New System.Drawing.Point(193, 40)
+        Me.P3.Name = "P3"
+        Me.P3.Size = New System.Drawing.Size(68, 67)
+        Me.P3.TabIndex = 2
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(267, 40)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(68, 67)
+        Me.Panel1.TabIndex = 3
+        '
+        'Panel2
+        '
+        Me.Panel2.Location = New System.Drawing.Point(341, 40)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(68, 67)
+        Me.Panel2.TabIndex = 3
+        '
+        'Panel3
+        '
+        Me.Panel3.Location = New System.Drawing.Point(45, 332)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(68, 67)
+        Me.Panel3.TabIndex = 4
+        '
+        'Panel4
+        '
+        Me.Panel4.Location = New System.Drawing.Point(45, 259)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(68, 67)
+        Me.Panel4.TabIndex = 4
+        '
+        'Panel5
+        '
+        Me.Panel5.Location = New System.Drawing.Point(45, 186)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(68, 67)
+        Me.Panel5.TabIndex = 4
+        '
+        'Panel6
+        '
+        Me.Panel6.Location = New System.Drawing.Point(45, 113)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(68, 67)
+        Me.Panel6.TabIndex = 4
+        '
+        'Panel7
+        '
+        Me.Panel7.Location = New System.Drawing.Point(119, 113)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(68, 67)
+        Me.Panel7.TabIndex = 5
+        '
+        'Panel8
+        '
+        Me.Panel8.Location = New System.Drawing.Point(193, 113)
+        Me.Panel8.Name = "Panel8"
+        Me.Panel8.Size = New System.Drawing.Size(68, 67)
+        Me.Panel8.TabIndex = 5
+        '
+        'Panel9
+        '
+        Me.Panel9.Location = New System.Drawing.Point(267, 113)
+        Me.Panel9.Name = "Panel9"
+        Me.Panel9.Size = New System.Drawing.Size(68, 67)
+        Me.Panel9.TabIndex = 5
+        '
+        'Panel10
+        '
+        Me.Panel10.Location = New System.Drawing.Point(341, 113)
+        Me.Panel10.Name = "Panel10"
+        Me.Panel10.Size = New System.Drawing.Size(68, 67)
+        Me.Panel10.TabIndex = 5
+        '
+        'Panel11
+        '
+        Me.Panel11.Location = New System.Drawing.Point(119, 186)
+        Me.Panel11.Name = "Panel11"
+        Me.Panel11.Size = New System.Drawing.Size(68, 67)
+        Me.Panel11.TabIndex = 5
+        '
+        'Panel12
+        '
+        Me.Panel12.Location = New System.Drawing.Point(193, 186)
+        Me.Panel12.Name = "Panel12"
+        Me.Panel12.Size = New System.Drawing.Size(68, 67)
+        Me.Panel12.TabIndex = 5
+        '
+        'Panel13
+        '
+        Me.Panel13.Location = New System.Drawing.Point(267, 186)
+        Me.Panel13.Name = "Panel13"
+        Me.Panel13.Size = New System.Drawing.Size(68, 67)
+        Me.Panel13.TabIndex = 5
+        '
+        'Panel14
+        '
+        Me.Panel14.Location = New System.Drawing.Point(341, 186)
+        Me.Panel14.Name = "Panel14"
+        Me.Panel14.Size = New System.Drawing.Size(68, 67)
+        Me.Panel14.TabIndex = 5
+        '
+        'Panel15
+        '
+        Me.Panel15.Location = New System.Drawing.Point(119, 259)
+        Me.Panel15.Name = "Panel15"
+        Me.Panel15.Size = New System.Drawing.Size(68, 67)
+        Me.Panel15.TabIndex = 5
+        '
+        'Panel16
+        '
+        Me.Panel16.Location = New System.Drawing.Point(193, 259)
+        Me.Panel16.Name = "Panel16"
+        Me.Panel16.Size = New System.Drawing.Size(68, 67)
+        Me.Panel16.TabIndex = 5
+        '
+        'Panel17
+        '
+        Me.Panel17.Location = New System.Drawing.Point(267, 259)
+        Me.Panel17.Name = "Panel17"
+        Me.Panel17.Size = New System.Drawing.Size(68, 67)
+        Me.Panel17.TabIndex = 5
+        '
+        'Panel18
+        '
+        Me.Panel18.Location = New System.Drawing.Point(341, 259)
+        Me.Panel18.Name = "Panel18"
+        Me.Panel18.Size = New System.Drawing.Size(68, 67)
+        Me.Panel18.TabIndex = 5
+        '
+        'Panel19
+        '
+        Me.Panel19.Location = New System.Drawing.Point(119, 332)
+        Me.Panel19.Name = "Panel19"
+        Me.Panel19.Size = New System.Drawing.Size(68, 67)
+        Me.Panel19.TabIndex = 5
+        '
+        'Panel20
+        '
+        Me.Panel20.Location = New System.Drawing.Point(193, 332)
+        Me.Panel20.Name = "Panel20"
+        Me.Panel20.Size = New System.Drawing.Size(68, 67)
+        Me.Panel20.TabIndex = 5
+        '
+        'Panel21
+        '
+        Me.Panel21.Location = New System.Drawing.Point(267, 332)
+        Me.Panel21.Name = "Panel21"
+        Me.Panel21.Size = New System.Drawing.Size(68, 67)
+        Me.Panel21.TabIndex = 5
+        '
+        'Panel22
+        '
+        Me.Panel22.Location = New System.Drawing.Point(341, 332)
+        Me.Panel22.Name = "Panel22"
+        Me.Panel22.Size = New System.Drawing.Size(68, 67)
+        Me.Panel22.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(455, 450)
+        Me.Controls.Add(Me.Panel19)
+        Me.Controls.Add(Me.Panel20)
+        Me.Controls.Add(Me.Panel21)
+        Me.Controls.Add(Me.Panel22)
+        Me.Controls.Add(Me.Panel15)
+        Me.Controls.Add(Me.Panel16)
+        Me.Controls.Add(Me.Panel17)
+        Me.Controls.Add(Me.Panel18)
+        Me.Controls.Add(Me.Panel11)
+        Me.Controls.Add(Me.Panel12)
+        Me.Controls.Add(Me.Panel13)
+        Me.Controls.Add(Me.Panel14)
+        Me.Controls.Add(Me.Panel7)
+        Me.Controls.Add(Me.Panel8)
+        Me.Controls.Add(Me.Panel9)
+        Me.Controls.Add(Me.Panel10)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel6)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.P3)
+        Me.Controls.Add(Me.P2)
+        Me.Controls.Add(Me.P1)
+        Me.Name = "Form1"
+        Me.Text = "7-Segment"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents P1 As Panel
+    Friend WithEvents P2 As Panel
+    Friend WithEvents P3 As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents Panel10 As Panel
+    Friend WithEvents Panel11 As Panel
+    Friend WithEvents Panel12 As Panel
+    Friend WithEvents Panel13 As Panel
+    Friend WithEvents Panel14 As Panel
+    Friend WithEvents Panel15 As Panel
+    Friend WithEvents Panel16 As Panel
+    Friend WithEvents Panel17 As Panel
+    Friend WithEvents Panel18 As Panel
+    Friend WithEvents Panel19 As Panel
+    Friend WithEvents Panel20 As Panel
+    Friend WithEvents Panel21 As Panel
+    Friend WithEvents Panel22 As Panel
+End Class
