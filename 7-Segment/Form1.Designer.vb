d06e40b5212465ec54cdf7bec163e7c355968db7
TriciaMae <58018345+TriciaMae@users.noreply.github.com>
2020-02-19 01:30:28 +0800

    Created 0-3 Design

--- 7-Segment/Form1.Designer.vb
+++ 7-Segment/Form1.Designer.vb
@@ -25,28 +25,28 @@ Partial Class Form1
         Me.P1 = New System.Windows.Forms.Panel()
         Me.P2 = New System.Windows.Forms.Panel()
         Me.P3 = New System.Windows.Forms.Panel()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Panel3 = New System.Windows.Forms.Panel()
-        Me.Panel4 = New System.Windows.Forms.Panel()
-        Me.Panel5 = New System.Windows.Forms.Panel()
-        Me.Panel6 = New System.Windows.Forms.Panel()
-        Me.Panel7 = New System.Windows.Forms.Panel()
-        Me.Panel8 = New System.Windows.Forms.Panel()
-        Me.Panel9 = New System.Windows.Forms.Panel()
-        Me.Panel10 = New System.Windows.Forms.Panel()
-        Me.Panel11 = New System.Windows.Forms.Panel()
-        Me.Panel12 = New System.Windows.Forms.Panel()
-        Me.Panel13 = New System.Windows.Forms.Panel()
-        Me.Panel14 = New System.Windows.Forms.Panel()
-        Me.Panel15 = New System.Windows.Forms.Panel()
-        Me.Panel16 = New System.Windows.Forms.Panel()
-        Me.Panel17 = New System.Windows.Forms.Panel()
-        Me.Panel18 = New System.Windows.Forms.Panel()
-        Me.Panel19 = New System.Windows.Forms.Panel()
-        Me.Panel20 = New System.Windows.Forms.Panel()
-        Me.Panel21 = New System.Windows.Forms.Panel()
-        Me.Panel22 = New System.Windows.Forms.Panel()
+        Me.P5 = New System.Windows.Forms.Panel()
+        Me.P4 = New System.Windows.Forms.Panel()
+        Me.P6 = New System.Windows.Forms.Panel()
+        Me.P11 = New System.Windows.Forms.Panel()
+        Me.P16 = New System.Windows.Forms.Panel()
+        Me.P21 = New System.Windows.Forms.Panel()
+        Me.P10 = New System.Windows.Forms.Panel()
+        Me.P9 = New System.Windows.Forms.Panel()
+        Me.P8 = New System.Windows.Forms.Panel()
+        Me.P7 = New System.Windows.Forms.Panel()
+        Me.P15 = New System.Windows.Forms.Panel()
+        Me.P14 = New System.Windows.Forms.Panel()
+        Me.P13 = New System.Windows.Forms.Panel()
+        Me.P12 = New System.Windows.Forms.Panel()
+        Me.P20 = New System.Windows.Forms.Panel()
+        Me.P19 = New System.Windows.Forms.Panel()
+        Me.P18 = New System.Windows.Forms.Panel()
+        Me.P17 = New System.Windows.Forms.Panel()
+        Me.P25 = New System.Windows.Forms.Panel()
+        Me.P24 = New System.Windows.Forms.Panel()
+        Me.P23 = New System.Windows.Forms.Panel()
+        Me.P22 = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
         '
         'P1
@@ -70,187 +70,187 @@ Partial Class Form1
         Me.P3.Size = New System.Drawing.Size(68, 67)
         Me.P3.TabIndex = 2
         '
-        'Panel1
+        'P5
         '
-        Me.Panel1.Location = New System.Drawing.Point(267, 40)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(68, 67)
-        Me.Panel1.TabIndex = 3
+        Me.P5.Location = New System.Drawing.Point(341, 40)
+        Me.P5.Name = "P5"
+        Me.P5.Size = New System.Drawing.Size(68, 67)
+        Me.P5.TabIndex = 3
         '
-        'Panel2
+        'P4
         '
-        Me.Panel2.Location = New System.Drawing.Point(341, 40)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(68, 67)
-        Me.Panel2.TabIndex = 3
+        Me.P4.Location = New System.Drawing.Point(267, 40)
+        Me.P4.Name = "P4"
+        Me.P4.Size = New System.Drawing.Size(68, 67)
+        Me.P4.TabIndex = 3
         '
-        'Panel3
+        'P6
         '
-        Me.Panel3.Location = New System.Drawing.Point(45, 332)
-        Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(68, 67)
-        Me.Panel3.TabIndex = 4
+        Me.P6.Location = New System.Drawing.Point(45, 113)
+        Me.P6.Name = "P6"
+        Me.P6.Size = New System.Drawing.Size(68, 67)
+        Me.P6.TabIndex = 4
         '
-        'Panel4
+        'P11
         '
-        Me.Panel4.Location = New System.Drawing.Point(45, 259)
-        Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(68, 67)
-        Me.Panel4.TabIndex = 4
+        Me.P11.Location = New System.Drawing.Point(45, 186)
+        Me.P11.Name = "P11"
+        Me.P11.Size = New System.Drawing.Size(68, 67)
+        Me.P11.TabIndex = 4
         '
-        'Panel5
+        'P16
         '
-        Me.Panel5.Location = New System.Drawing.Point(45, 186)
-        Me.Panel5.Name = "Panel5"
-        Me.Panel5.Size = New System.Drawing.Size(68, 67)
-        Me.Panel5.TabIndex = 4
+        Me.P16.Location = New System.Drawing.Point(45, 259)
+        Me.P16.Name = "P16"
+        Me.P16.Size = New System.Drawing.Size(68, 67)
+        Me.P16.TabIndex = 4
         '
-        'Panel6
+        'P21
         '
-        Me.Panel6.Location = New System.Drawing.Point(45, 113)
-        Me.Panel6.Name = "Panel6"
-        Me.Panel6.Size = New System.Drawing.Size(68, 67)
-        Me.Panel6.TabIndex = 4
+        Me.P21.Location = New System.Drawing.Point(45, 332)
+        Me.P21.Name = "P21"
+        Me.P21.Size = New System.Drawing.Size(68, 67)
+        Me.P21.TabIndex = 4
         '
-        'Panel7
+        'P10
         '
-        Me.Panel7.Location = New System.Drawing.Point(119, 113)
-        Me.Panel7.Name = "Panel7"
-        Me.Panel7.Size = New System.Drawing.Size(68, 67)
-        Me.Panel7.TabIndex = 5
+        Me.P10.Location = New System.Drawing.Point(341, 113)
+        Me.P10.Name = "P10"
+        Me.P10.Size = New System.Drawing.Size(68, 67)
+        Me.P10.TabIndex = 5
         '
-        'Panel8
+        'P9
         '
-        Me.Panel8.Location = New System.Drawing.Point(193, 113)
-        Me.Panel8.Name = "Panel8"
-        Me.Panel8.Size = New System.Drawing.Size(68, 67)
-        Me.Panel8.TabIndex = 5
+        Me.P9.Location = New System.Drawing.Point(267, 113)
+        Me.P9.Name = "P9"
+        Me.P9.Size = New System.Drawing.Size(68, 67)
+        Me.P9.TabIndex = 5
         '
-        'Panel9
+        'P8
         '
-        Me.Panel9.Location = New System.Drawing.Point(267, 113)
-        Me.Panel9.Name = "Panel9"
-        Me.Panel9.Size = New System.Drawing.Size(68, 67)
-        Me.Panel9.TabIndex = 5
+        Me.P8.Location = New System.Drawing.Point(193, 113)
+        Me.P8.Name = "P8"
+        Me.P8.Size = New System.Drawing.Size(68, 67)
+        Me.P8.TabIndex = 5
         '
-        'Panel10
+        'P7
         '
-        Me.Panel10.Location = New System.Drawing.Point(341, 113)
-        Me.Panel10.Name = "Panel10"
-        Me.Panel10.Size = New System.Drawing.Size(68, 67)
-        Me.Panel10.TabIndex = 5
+        Me.P7.Location = New System.Drawing.Point(119, 113)
+        Me.P7.Name = "P7"
+        Me.P7.Size = New System.Drawing.Size(68, 67)
+        Me.P7.TabIndex = 5
         '
-        'Panel11
+        'P15
         '
-        Me.Panel11.Location = New System.Drawing.Point(119, 186)
-        Me.Panel11.Name = "Panel11"
-        Me.Panel11.Size = New System.Drawing.Size(68, 67)
-        Me.Panel11.TabIndex = 5
+        Me.P15.Location = New System.Drawing.Point(341, 186)
+        Me.P15.Name = "P15"
+        Me.P15.Size = New System.Drawing.Size(68, 67)
+        Me.P15.TabIndex = 5
         '
-        'Panel12
+        'P14
         '
-        Me.Panel12.Location = New System.Drawing.Point(193, 186)
-        Me.Panel12.Name = "Panel12"
-        Me.Panel12.Size = New System.Drawing.Size(68, 67)
-        Me.Panel12.TabIndex = 5
+        Me.P14.Location = New System.Drawing.Point(267, 186)
+        Me.P14.Name = "P14"
+        Me.P14.Size = New System.Drawing.Size(68, 67)
+        Me.P14.TabIndex = 5
         '
-        'Panel13
+        'P13
         '
-        Me.Panel13.Location = New System.Drawing.Point(267, 186)
-        Me.Panel13.Name = "Panel13"
-        Me.Panel13.Size = New System.Drawing.Size(68, 67)
-        Me.Panel13.TabIndex = 5
+        Me.P13.Location = New System.Drawing.Point(193, 186)
+        Me.P13.Name = "P13"
+        Me.P13.Size = New System.Drawing.Size(68, 67)
+        Me.P13.TabIndex = 5
         '
-        'Panel14
+        'P12
         '
-        Me.Panel14.Location = New System.Drawing.Point(341, 186)
-        Me.Panel14.Name = "Panel14"
-        Me.Panel14.Size = New System.Drawing.Size(68, 67)
-        Me.Panel14.TabIndex = 5
+        Me.P12.Location = New System.Drawing.Point(119, 186)
+        Me.P12.Name = "P12"
+        Me.P12.Size = New System.Drawing.Size(68, 67)
+        Me.P12.TabIndex = 5
         '
-        'Panel15
+        'P20
         '
-        Me.Panel15.Location = New System.Drawing.Point(119, 259)
-        Me.Panel15.Name = "Panel15"
-        Me.Panel15.Size = New System.Drawing.Size(68, 67)
-        Me.Panel15.TabIndex = 5
+        Me.P20.Location = New System.Drawing.Point(341, 259)
+        Me.P20.Name = "P20"
+        Me.P20.Size = New System.Drawing.Size(68, 67)
+        Me.P20.TabIndex = 5
         '
-        'Panel16
+        'P19
         '
-        Me.Panel16.Location = New System.Drawing.Point(193, 259)
-        Me.Panel16.Name = "Panel16"
-        Me.Panel16.Size = New System.Drawing.Size(68, 67)
-        Me.Panel16.TabIndex = 5
+        Me.P19.Location = New System.Drawing.Point(267, 259)
+        Me.P19.Name = "P19"
+        Me.P19.Size = New System.Drawing.Size(68, 67)
+        Me.P19.TabIndex = 5
         '
-        'Panel17
+        'P18
         '
-        Me.Panel17.Location = New System.Drawing.Point(267, 259)
-        Me.Panel17.Name = "Panel17"
-        Me.Panel17.Size = New System.Drawing.Size(68, 67)
-        Me.Panel17.TabIndex = 5
+        Me.P18.Location = New System.Drawing.Point(193, 259)
+        Me.P18.Name = "P18"
+        Me.P18.Size = New System.Drawing.Size(68, 67)
+        Me.P18.TabIndex = 5
         '
-        'Panel18
+        'P17
         '
-        Me.Panel18.Location = New System.Drawing.Point(341, 259)
-        Me.Panel18.Name = "Panel18"
-        Me.Panel18.Size = New System.Drawing.Size(68, 67)
-        Me.Panel18.TabIndex = 5
+        Me.P17.Location = New System.Drawing.Point(119, 259)
+        Me.P17.Name = "P17"
+        Me.P17.Size = New System.Drawing.Size(68, 67)
+        Me.P17.TabIndex = 5
         '
-        'Panel19
+        'P25
         '
-        Me.Panel19.Location = New System.Drawing.Point(119, 332)
-        Me.Panel19.Name = "Panel19"
-        Me.Panel19.Size = New System.Drawing.Size(68, 67)
-        Me.Panel19.TabIndex = 5
+        Me.P25.Location = New System.Drawing.Point(341, 332)
+        Me.P25.Name = "P25"
+        Me.P25.Size = New System.Drawing.Size(68, 67)
+        Me.P25.TabIndex = 5
         '
-        'Panel20
+        'P24
         '
-        Me.Panel20.Location = New System.Drawing.Point(193, 332)
-        Me.Panel20.Name = "Panel20"
-        Me.Panel20.Size = New System.Drawing.Size(68, 67)
-        Me.Panel20.TabIndex = 5
+        Me.P24.Location = New System.Drawing.Point(267, 332)
+        Me.P24.Name = "P24"
+        Me.P24.Size = New System.Drawing.Size(68, 67)
+        Me.P24.TabIndex = 5
         '
-        'Panel21
+        'P23
         '
-        Me.Panel21.Location = New System.Drawing.Point(267, 332)
-        Me.Panel21.Name = "Panel21"
-        Me.Panel21.Size = New System.Drawing.Size(68, 67)
-        Me.Panel21.TabIndex = 5
+        Me.P23.Location = New System.Drawing.Point(193, 332)
+        Me.P23.Name = "P23"
+        Me.P23.Size = New System.Drawing.Size(68, 67)
+        Me.P23.TabIndex = 5
         '
-        'Panel22
+        'P22
         '
-        Me.Panel22.Location = New System.Drawing.Point(341, 332)
-        Me.Panel22.Name = "Panel22"
-        Me.Panel22.Size = New System.Drawing.Size(68, 67)
-        Me.Panel22.TabIndex = 5
+        Me.P22.Location = New System.Drawing.Point(119, 332)
+        Me.P22.Name = "P22"
+        Me.P22.Size = New System.Drawing.Size(68, 67)
+        Me.P22.TabIndex = 5
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(455, 450)
-        Me.Controls.Add(Me.Panel19)
-        Me.Controls.Add(Me.Panel20)
-        Me.Controls.Add(Me.Panel21)
-        Me.Controls.Add(Me.Panel22)
-        Me.Controls.Add(Me.Panel15)
-        Me.Controls.Add(Me.Panel16)
-        Me.Controls.Add(Me.Panel17)
-        Me.Controls.Add(Me.Panel18)
-        Me.Controls.Add(Me.Panel11)
-        Me.Controls.Add(Me.Panel12)
-        Me.Controls.Add(Me.Panel13)
-        Me.Controls.Add(Me.Panel14)
-        Me.Controls.Add(Me.Panel7)
-        Me.Controls.Add(Me.Panel8)
-        Me.Controls.Add(Me.Panel9)
-        Me.Controls.Add(Me.Panel10)
-        Me.Controls.Add(Me.Panel3)
-        Me.Controls.Add(Me.Panel4)
-        Me.Controls.Add(Me.Panel5)
-        Me.Controls.Add(Me.Panel6)
-        Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.P22)
+        Me.Controls.Add(Me.P23)
+        Me.Controls.Add(Me.P24)
+        Me.Controls.Add(Me.P25)
+        Me.Controls.Add(Me.P17)
+        Me.Controls.Add(Me.P18)
+        Me.Controls.Add(Me.P19)
+        Me.Controls.Add(Me.P20)
+        Me.Controls.Add(Me.P12)
+        Me.Controls.Add(Me.P13)
+        Me.Controls.Add(Me.P14)
+        Me.Controls.Add(Me.P15)
+        Me.Controls.Add(Me.P7)
+        Me.Controls.Add(Me.P8)
+        Me.Controls.Add(Me.P9)
+        Me.Controls.Add(Me.P10)
+        Me.Controls.Add(Me.P21)
+        Me.Controls.Add(Me.P16)
+        Me.Controls.Add(Me.P11)
+        Me.Controls.Add(Me.P6)
+        Me.Controls.Add(Me.P4)
+        Me.Controls.Add(Me.P5)
         Me.Controls.Add(Me.P3)
         Me.Controls.Add(Me.P2)
         Me.Controls.Add(Me.P1)
@@ -259,29 +259,30 @@ Partial Class Form1
         Me.ResumeLayout(False)
 
     End Sub
+
     Friend WithEvents P1 As Panel
     Friend WithEvents P2 As Panel
     Friend WithEvents P3 As Panel
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents Panel2 As Panel
-    Friend WithEvents Panel3 As Panel
-    Friend WithEvents Panel4 As Panel
-    Friend WithEvents Panel5 As Panel
-    Friend WithEvents Panel6 As Panel
-    Friend WithEvents Panel7 As Panel
-    Friend WithEvents Panel8 As Panel
-    Friend WithEvents Panel9 As Panel
-    Friend WithEvents Panel10 As Panel
-    Friend WithEvents Panel11 As Panel
-    Friend WithEvents Panel12 As Panel
-    Friend WithEvents Panel13 As Panel
-    Friend WithEvents Panel14 As Panel
-    Friend WithEvents Panel15 As Panel
-    Friend WithEvents Panel16 As Panel
-    Friend WithEvents Panel17 As Panel
-    Friend WithEvents Panel18 As Panel
-    Friend WithEvents Panel19 As Panel
-    Friend WithEvents Panel20 As Panel
-    Friend WithEvents Panel21 As Panel
-    Friend WithEvents Panel22 As Panel
+    Friend WithEvents P5 As Panel
+    Friend WithEvents P4 As Panel
+    Friend WithEvents P6 As Panel
+    Friend WithEvents P11 As Panel
+    Friend WithEvents P16 As Panel
+    Friend WithEvents P21 As Panel
+    Friend WithEvents P10 As Panel
+    Friend WithEvents P9 As Panel
+    Friend WithEvents P8 As Panel
+    Friend WithEvents P7 As Panel
+    Friend WithEvents P15 As Panel
+    Friend WithEvents P14 As Panel
+    Friend WithEvents P13 As Panel
+    Friend WithEvents P12 As Panel
+    Friend WithEvents P20 As Panel
+    Friend WithEvents P19 As Panel
+    Friend WithEvents P18 As Panel
+    Friend WithEvents P17 As Panel
+    Friend WithEvents P25 As Panel
+    Friend WithEvents P24 As Panel
+    Friend WithEvents P23 As Panel
+    Friend WithEvents P22 As Panel
 End Class
--- 7-Segment/Form1.vb
+++ 7-Segment/Form1.vb
@@ -1,7 +1,82 @@
 ﻿Public Class Form1
 
+    'for the delay
+    Private Sub Hold(ByVal interval As Integer)
+        Dim wait As New Stopwatch
+        wait.Start()
+
+        Do While wait.ElapsedMilliseconds < interval
+            Application.DoEvents()
+        Loop
+
+        wait.Stop()
+    End Sub
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Do
+            '0
+            Hold(1000)
+            P1.BackColor = Color.Red
+            P2.BackColor = Color.Red
+            P3.BackColor = Color.Red
+            P4.BackColor = Color.Red
+            P5.BackColor = Color.Red
+            P6.BackColor = Color.Red
+            P7.BackColor = Color.White
+            P8.BackColor = Color.White
+            P9.BackColor = Color.White
+            P10.BackColor = Color.Red
+            P11.BackColor = Color.Red
+            P12.BackColor = Color.White
+            P13.BackColor = Color.White
+            P14.BackColor = Color.White
+            P15.BackColor = Color.Red
+            P16.BackColor = Color.Red
+            P17.BackColor = Color.White
+            P18.BackColor = Color.White
+            P19.BackColor = Color.White
+            P20.BackColor = Color.Red
+            P21.BackColor = Color.Red
+            P22.BackColor = Color.Red
+            P23.BackColor = Color.Red
+            P24.BackColor = Color.Red
+            P25.BackColor = Color.Red
 
+            '1
+            Hold(1000)
+            P1.BackColor = Color.White
+            P4.BackColor = Color.White
+            P5.BackColor = Color.White
+            P8.BackColor = Color.Red
+            P10.BackColor = Color.White
+            P11.BackColor = Color.White
+            P13.BackColor = Color.Red
+            P15.BackColor = Color.White
+            P16.BackColor = Color.White
+            P18.BackColor = Color.Red
+            P20.BackColor = Color.White
 
+            '2
+            Hold(1000)
+            P1.BackColor = Color.Red
+            P4.BackColor = Color.Red
+            P5.BackColor = Color.Red
+            P6.BackColor = Color.White
+            P8.BackColor = Color.White
+            P10.BackColor = Color.Red
+            P11.BackColor = Color.Red
+            P12.BackColor = Color.Red
+            P14.BackColor = Color.Red
+            P15.BackColor = Color.Red
+            P16.BackColor = Color.Red
+            P18.BackColor = Color.White
+
+            '3
+            Hold(1000)
+            P11.BackColor = Color.White
+            P12.BackColor = Color.White
+            P16.BackColor = Color.White
+            P20.BackColor = Color.Red
+
+        Loop While True
     End Sub
 End Class
